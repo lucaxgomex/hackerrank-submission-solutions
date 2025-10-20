b95fd5306c1fa6cc3a6c49b49acb99cad0f02069
lucaxgomex <lg.projectdev@gmail.com>
2025-10-20 17:30:49 -0300

    ruby: range over the entire matrix

--- problem-solving/medium/forming-a-magic-square/ruby.rb
+++ problem-solving/medium/forming-a-magic-square/ruby.rb
@@ -6,6 +6,17 @@ matrix = Array.new(3){
 matrix[0][2] = 6
 
 puts matrix
+
+
+   d1 = Array.new(3)
+
+    
+    3.times do |i|
+      d1[i] = a[i][i - 1]
+      r = r + d1[i]
+    end
+  
+    puts is_even(16)
 =end
 
 #!/bin/ruby
@@ -18,32 +29,43 @@ require 'stringio'
 # The function is expected to return an INTEGER.
 # The function accepts 2D_INTEGER_ARRAY s as parameter.
 #
+#
+
+def diagonalCalculation(i, j):
+end
 
 def formingMagicSquare(s)
-  if s[1][1] != 5
+  d1 = s[0][0] + s[1][1] + s[2][2]
+  d2 = s[2][0] + s[1][1] + s[0][2]
+
+  h1 = s[0][0] + s[0][1] + s[0][2]
+  h2 = s[1][0] + s[1][1] + s[1][2]
+  h3 = s[2][0] + s[2][1] + s[2][2]
+
+  cost = 0
+
+  if s[1][1] != 5 then
     raise 'You cannot go further! This value must be 5'
   else
-    # diagonal rows
-    d1 = Array.new(3)
-    r = 0
-    
-    3.times do |i|
-      d1[i] = a[i][i - 1]
-      r = r + d1[i]
+    if d1 < 15 then
+      cost = 15 - d1     
+      
+      if s[0][0].odd? == true then
+        s[0][0] += cost   
+      elsif a[2][2].odd? == true then
+        s[2][2] += cost
+      end
+        
+    else
+      cost = d1 - 15
     end
-  
-  
-  
-    d2 = Array.new(3)
-    
-    # vertical rows
-    v1 = Array.new(3)
-    v2 = Array.new(3)
-    v3 = Array.new(3)
-    
-    puts is_even(16)
 
-  end
+    if d2 < 15 then
+      cost = 15 - d2
+    else
+      cost = d2 - 15
+    end
+  end    
 end
 
 
