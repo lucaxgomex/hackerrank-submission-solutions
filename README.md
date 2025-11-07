33dd7938a3d06c4a10bc3a355da2c42e4e80036f
lucaxgomex <lg.projectdev@gmail.com>
2025-11-07 17:27:10 -0300

    staircase: problem solved in C#

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+![Company Logo](https://hrcdn.net/fcore/assets/brand/logo-new-white-green-a5cb16e0ae.svg "Example Company Logo")
+
+You will be able to check out my HackerRank profile through the URL below. By clicking it, it's possible figure out what kind of technologies and programming languages I'm up to, together with the problems I'm solving.
+
 ![lucaxgomex's Stats](https://github-readme-stats.vercel.app/api?username=lucaxgomex&theme=outrun&show_icons=true&hide_border=false&count_private=true)
 
 ![lucaxgomex's Streak](https://github-readme-streak-stats.herokuapp.com/?user=lucaxgomex&theme=outrun&hide_border=false)
---
+++ problem-solving/easy/staircase/csharp.cs
@@ -0,0 +1,48 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+    public static void staircase(int n)
+    {
+        int spaceCount = n - 1;
+
+        for (int i = 1; i < (n + 1); i++)
+        {   
+            string spaceRepeat = new string(' ', spaceCount);
+            string varRepeat = new string('#', i);
+
+            Console.WriteLine(string.Concat(spaceRepeat, varRepeat));
+
+            spaceCount--;
+        }
+    }
+
+    public static string Reverse(string s)
+    {
+        char[] charArray = s.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        Result.staircase(n);
+    }
+}
