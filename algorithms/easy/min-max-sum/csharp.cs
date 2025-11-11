e4058049d0709e95c66f5b900b1f0b3a38d910ef
lucaxgomex <lg.projectdev@gmail.com>
2025-11-11 14:50:01 -0300

    min max sum: solved in C#

---
+++ algorithms/easy/min-max-sum/csharp.cs
@@ -0,0 +1,47 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+    public static void miniMaxSum(List<int> arr)
+    {
+        //int minSum = 0;
+        //int maxSum = 0;
+        long s = 0;
+        List<long> newResult = new List<long>();
+        int arrSize = arr.Count;
+
+        for (int i = 0; i < arrSize; i++)
+        {
+            s += arr[i];
+        }
+
+        for (int j = 0; j < arrSize; j++)
+        {
+            newResult.Add(s - arr[j]);
+        }
+
+        Console.WriteLine($"{newResult.Min()} {newResult.Max()}");
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Result.miniMaxSum(arr);
+    }
+}
