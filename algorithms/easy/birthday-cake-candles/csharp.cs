cbba9a1cdf56c170c8dc5a847d542b8e8c3551e6
lucaxgomex <lg.projectdev@gmail.com>
2025-11-11 16:55:03 -0300

    added birthday cake candles problem foldder

---
+++ algorithms/easy/birthday-cake-candles/csharp.cs
@@ -0,0 +1,53 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+    public static int birthdayCakeCandles(List<int> candles)
+    {
+        int x = 0;
+        //int y = 0;
+        //int q = 0;
+
+        for (int i = 0; i < candles.Count; i++)
+        {
+            if (x < candles[i])
+            {
+                x = candles[i];
+            }
+        }
+        return x;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+        //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int candlesCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> candles = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(candlesTemp => Convert.ToInt32(candlesTemp)).ToList();
+
+        int result = Result.birthdayCakeCandles(candles);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
